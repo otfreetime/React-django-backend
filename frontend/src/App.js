2536374697a414d66771f1ef2e6de99e3796cb59
otfreetime <omaraltaezi@gmail.com>
2022-02-15 21:44:27 +0300

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello Wold
         </p>
         <a
-          className="App-link"
+          className="Hello"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
