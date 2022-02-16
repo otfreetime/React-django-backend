76616e0ba58572baf70685729a0220656eacfaad
otfreetime <omaraltaezi@gmail.com>
2022-02-16 23:49:13 +0300

    Update

--- rootapp/api/views.py
+++ rootapp/api/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return HttpResponse('Hello Word from Omar')
+    return HttpResponse('Hello Word from Omar a')
